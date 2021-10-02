82f552738fb1361ce2d8d92966deb27eb1d24b58
AntonioAlzate <antonioalzate7@gmail.com>
2021-10-02 01:33:00 -0500

    Se agrego el paquete de Microsoft.EntityFrameworkCore.Sqlite y se agrego el contexto de datos para el tema de persistencia y que genere el archivo de bd sqlite  y realice las migraciones a ese archivo.

---
+++ PreguntasRespuestasApp/Persistencia/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreguntasRespuestasApp.Persistencia
+{
+    public class DatabaseContext : DbContext
+    {
+        private string nameDb = "preguntas_respuestas.db";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(connectionString: $"FileName={nameDb}", sqliteOptionsAction: option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        // DbSets
+    }
+}
